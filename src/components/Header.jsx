b7b67f2012ec0fcd24b0fc9b5d1340f053bd1cea
Colin Moen <cdouglasmoen@gmail.com>
2026-02-12 16:14:00 -0800

    create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header({ user, onLogout }) {
+  return (
+    <header>
+      <h1>Todo List App</h1>
+      <div className="auth-status">
+        <span>{user ? `Logged in as ${user}` : "Not logged in"}</span>
+        <button onClick={onLogout} disabled={!user}>
+          Logout
+        </button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
